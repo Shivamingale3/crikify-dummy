72bd3ca328e21fb31869a6578cc6dbc11d000fed
Shivamingale3 <shivamingale3@gmail.com>
2026-01-06 11:24:54 +0530

    style: reduce background color opacity for non-liquid glass supported elements.

--- app/index.tsx
+++ app/index.tsx
@@ -22,7 +22,7 @@ const HomeScreen = () => {
             style={[
               { width: "100%", borderRadius: 20, borderWidth: 2, padding: 20 },
               !isLiquidGlassSupported && {
-                backgroundColor: "rgba(255,255,255,0.3)",
+                backgroundColor: "rgba(255,255,255,0.2)",
                 flexDirection: "row",
                 alignItems: "center",
               },
